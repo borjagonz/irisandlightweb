5a9fc5b79c1ed4ea18d4615c5a311dff3a77d969
borjagonz <borjagonz@gmail.com>
2026-07-21 12:01:51 +0200

    hola

--- src/components/MirandaComponent.vue
+++ src/components/MirandaComponent.vue
@@ -146,6 +146,7 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 
 import heroImage from '@/images/hero-dust-dissolve.jpg'
 import singlePortrait from '@/images/single-portrait.jpg'
@@ -153,6 +154,82 @@ import diagonalDuo from '@/images/diagonal-duo.jpg'
 import couplesInfinity from '@/images/couples-infinity.jpg'
 import familyGrid from '@/images/family-grid.jpg'
 
+onMounted(() => {
+  if (window.ttq) {
+    window.ttq.page()
+    return
+  }
+
+  window.TiktokAnalyticsObject = 'ttq'
+
+  const ttq = window.ttq = window.ttq || []
+
+  ttq.methods = [
+    'page',
+    'track',
+    'identify',
+    'instances',
+    'debug',
+    'on',
+    'off',
+    'once',
+    'ready',
+    'alias',
+    'group',
+    'enableCookie',
+    'disableCookie',
+    'holdConsent',
+    'revokeConsent',
+    'grantConsent'
+  ]
+
+  ttq.setAndDefer = function (t, e) {
+    t[e] = function () {
+      t.push([e].concat(Array.prototype.slice.call(arguments, 0)))
+    }
+  }
+
+  for (let i = 0; i < ttq.methods.length; i++) {
+    ttq.setAndDefer(ttq, ttq.methods[i])
+  }
+
+  ttq.instance = function (t) {
+    const e = ttq._i[t] || []
+    for (let n = 0; n < ttq.methods.length; n++) {
+      ttq.setAndDefer(e, ttq.methods[n])
+    }
+    return e
+  }
+
+  ttq.load = function (e, n) {
+    const r = 'https://analytics.tiktok.com/i18n/pixel/events.js'
+
+    ttq._i = ttq._i || {}
+    ttq._i[e] = []
+    ttq._i[e]._u = r
+
+    ttq._t = ttq._t || {}
+    ttq._t[e] = +new Date()
+
+    ttq._o = ttq._o || {}
+    ttq._o[e] = n || {}
+
+    const script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.async = true
+    script.src = `${r}?sdkid=${e}&lib=ttq`
+
+    const firstScript = document.getElementsByTagName('script')[0]
+    firstScript.parentNode.insertBefore(script, firstScript)
+  }
+
+  ttq.load('D98HBDRC77UBS5FS8LLG')
+  ttq.page()
+})
+
+window.ttq.track('ClickButton', {
+  button_name: 'Book your session'
+})
 </script>
 
 <style>
